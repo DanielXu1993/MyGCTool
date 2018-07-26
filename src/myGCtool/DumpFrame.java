5a0592f20affdceddb950c5d91cf0afac4d8695c
“Yonghong <540402187@qq.com>
2018-07-26 17:48:54 +0100

    tiny change

--- src/myGCtool/DumpFrame.java
+++ src/myGCtool/DumpFrame.java
@@ -3,6 +3,8 @@ package myGCtool;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.swing.BorderFactory;
@@ -21,15 +23,21 @@ public class DumpFrame extends JFrame
         this.setLocation(410, 240);
         
         JPanel centerPan = new JPanel(new BorderLayout());
-        centerPan.add(new JLabel("The largest 10 objects:"), BorderLayout.NORTH);
+        JPanel labelPan = new JPanel(new BorderLayout());
+        labelPan.add(new JLabel("The largest 10 objects:"), BorderLayout.WEST);
+        labelPan.add(
+            new JLabel("Time : "
+                + DateTimeFormatter.ofPattern(" HH:mm:ss ").format(LocalTime.now())),
+            BorderLayout.EAST);
+        centerPan.add(labelPan, BorderLayout.NORTH);
         JPanel tablePan = new JPanel(new GridLayout(0, 1));
         
         for (int i = 0; i < pids.size(); i++)
         {
             JPanel innerPan = new JPanel(new BorderLayout());
             innerPan.setBorder(BorderFactory.createLineBorder(Color.black));
-            JPanel labelPan = new JPanel(new BorderLayout());
-            labelPan.add(new JLabel(pids.get(i) + " : " + names.get(i)),
+            JPanel titlePan = new JPanel(new BorderLayout());
+            titlePan.add(new JLabel(pids.get(i) + " : " + names.get(i)),
                 BorderLayout.NORTH);
             String[] headings =
                 {" ", "instances(percentage%)", "size(byte)(percentage%)", "class name"};
@@ -40,8 +48,8 @@ public class DumpFrame extends JFrame
             table.getColumnModel().getColumn(1).setPreferredWidth(130);
             table.getColumnModel().getColumn(2).setPreferredWidth(130);
             table.getColumnModel().getColumn(3).setPreferredWidth(290);
-            labelPan.add(table.getTableHeader(), BorderLayout.SOUTH);
-            innerPan.add(labelPan, BorderLayout.NORTH);
+            titlePan.add(table.getTableHeader(), BorderLayout.SOUTH);
+            innerPan.add(titlePan, BorderLayout.NORTH);
             innerPan.add(table);
             tablePan.add(innerPan);
         }
