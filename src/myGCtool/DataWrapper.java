fdef2392e33325f411224ad140a0f839d04460ff
“Yonghong <540402187@qq.com>
2018-08-02 20:15:10 +0100

    fix bug

--- src/myGCtool/DataWrapper.java
+++ src/myGCtool/DataWrapper.java
@@ -26,40 +26,40 @@ public class DataWrapper
     private ArrayList<Date> timeList = new ArrayList<Date>();
     
     // store heap capacity, the total of the s0 capacity,s1 capacity,eden capacity and old generation capacity
-    private ArrayList<Integer> heapCapacity = new ArrayList<>();
+    private ArrayList<Double> heapCapacity = new ArrayList<>();
     
     // store heap usage, the total of the s0 usage,s1 usage,eden usage and old generation usage
-    private ArrayList<Integer> heapUsage = new ArrayList<>();
+    private ArrayList<Double> heapUsage = new ArrayList<>();
     
     // store S0 space capacity,the second column of data in the data file
-    private ArrayList<Integer> s0Capacity = new ArrayList<Integer>();
+    private ArrayList<Double> s0Capacity = new ArrayList<Double>();
     
     // store S1 space capacity,the 3rd column of data in the data file
-    private ArrayList<Integer> s1Capacity = new ArrayList<Integer>();
+    private ArrayList<Double> s1Capacity = new ArrayList<Double>();
     
     // store S0 space usage,the 4th column of data in the data file
-    private ArrayList<Integer> s0Usage = new ArrayList<Integer>();
+    private ArrayList<Double> s0Usage = new ArrayList<Double>();
     
     // store S1 space usage,the 5th column of data in the data file
-    private ArrayList<Integer> s1Usage = new ArrayList<Integer>();
+    private ArrayList<Double> s1Usage = new ArrayList<Double>();
     
     // store eden space capacity,the 6th column of data in the data file
-    private ArrayList<Integer> edenCapacity = new ArrayList<Integer>();
+    private ArrayList<Double> edenCapacity = new ArrayList<Double>();
     
     // store eden space usage,the 7th column of data in the data file
-    private ArrayList<Integer> edenUsage = new ArrayList<Integer>();
+    private ArrayList<Double> edenUsage = new ArrayList<Double>();
     
     // store old generation capacity,the 8th column of data in the data file
-    private ArrayList<Integer> oldCapacity = new ArrayList<Integer>();
+    private ArrayList<Double> oldCapacity = new ArrayList<Double>();
     
     // store old generation usage,the 9th column of data in the data file
-    private ArrayList<Integer> oldUsage = new ArrayList<Integer>();
+    private ArrayList<Double> oldUsage = new ArrayList<Double>();
     
     // store meta space capacity,the 10th column of data in the data file
-    private ArrayList<Integer> metaCapacity = new ArrayList<Integer>();
+    private ArrayList<Double> metaCapacity = new ArrayList<Double>();
     
     // store meta space usage,the 11th column of data in the data file
-    private ArrayList<Integer> metaUsage = new ArrayList<Integer>();
+    private ArrayList<Double> metaUsage = new ArrayList<Double>();
     
     // an array to store last 5 columns data in data line
     private double[] GCInfo = new double[5];
@@ -82,9 +82,8 @@ public class DataWrapper
     
     /**
      * Add formatted data to ArrayList
-     * Synchronized method ensures that no data interference occurs when monitoring multiple processes.
      */
-    public synchronized void addDataToList()
+    public void addDataToList()
     {
         // All data in the dataLine has been processed
         // waiting for the read thread to add data to the dataLine.
@@ -94,6 +93,8 @@ public class DataWrapper
             if (!Tools.isThreadRunning(pid))
                 return;
         }
+        if (Thread.currentThread().isInterrupted())
+            return;// jump out when current SwingWorker is cancelled
         int size = dataLines.size();
         // add new data to ArrayList
         for (int i = index; i < size; i++)
@@ -103,23 +104,22 @@ public class DataWrapper
             // add date
             timeList.add(new Date(Long.parseLong(data[0])));
             // add heap capacity,unit:MB
-            heapCapacity
-                .add((int)(Double.parseDouble(data[1]) + Double.parseDouble(data[2])
-                    + Double.parseDouble(data[5]) + Double.parseDouble(data[7])) / 1024);
+            heapCapacity.add((Double.parseDouble(data[1]) + Double.parseDouble(data[2])
+                + Double.parseDouble(data[5]) + Double.parseDouble(data[7])) / 1024);
             // add heap usage,unit:MB
-            heapUsage.add((int)(Double.parseDouble(data[3]) + Double.parseDouble(data[4])
+            heapUsage.add((Double.parseDouble(data[3]) + Double.parseDouble(data[4])
                 + Double.parseDouble(data[6]) + Double.parseDouble(data[8])) / 1024);
             
-            s0Capacity.add((int)Double.parseDouble(data[1]) / 1024);// add S0 capacity,unit:MB
-            s1Capacity.add((int)Double.parseDouble(data[2]) / 1024);// add S1 capacity,unit:MB
-            s0Usage.add((int)Double.parseDouble(data[3]) / 1024);// add S0 usage,unit:MB
-            s1Usage.add((int)Double.parseDouble(data[4]) / 1024);// add S1 usage,unit:MB
-            edenCapacity.add((int)Double.parseDouble(data[5]) / 1024);// add eden capacity,unit:MB
-            edenUsage.add((int)Double.parseDouble(data[6]) / 1024);// add eden usage,unit:MB
-            oldCapacity.add((int)Double.parseDouble(data[7]) / 1024);// add old generation capacity,unit:MB
-            oldUsage.add((int)Double.parseDouble(data[8]) / 1024);// add old generation usage,unit:MB
-            metaCapacity.add((int)Double.parseDouble(data[9]) / 1024);// add meta space capacity,unit:MB
-            metaUsage.add((int)Double.parseDouble(data[10]) / 1024);// add meta space usage,unit:MB
+            s0Capacity.add(Double.parseDouble(data[1]) / 1024);// add S0 capacity,unit:MB
+            s1Capacity.add(Double.parseDouble(data[2]) / 1024);// add S1 capacity,unit:MB
+            s0Usage.add(Double.parseDouble(data[3]) / 1024);// add S0 usage,unit:MB
+            s1Usage.add(Double.parseDouble(data[4]) / 1024);// add S1 usage,unit:MB
+            edenCapacity.add(Double.parseDouble(data[5]) / 1024);// add eden capacity,unit:MB
+            edenUsage.add(Double.parseDouble(data[6]) / 1024);// add eden usage,unit:MB
+            oldCapacity.add(Double.parseDouble(data[7]) / 1024);// add old generation capacity,unit:MB
+            oldUsage.add(Double.parseDouble(data[8]) / 1024);// add old generation usage,unit:MB
+            metaCapacity.add(Double.parseDouble(data[9]) / 1024);// add meta space capacity,unit:MB
+            metaUsage.add(Double.parseDouble(data[10]) / 1024);// add meta space usage,unit:MB
             GCInfo[0] = Double.parseDouble(data[13]);// add the count of minor GC
             GCInfo[1] = Double.parseDouble(data[14]);// add the time spent by minor GC
             GCInfo[2] = Double.parseDouble(data[15]);// add the count of full GC
